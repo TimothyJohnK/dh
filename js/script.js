8c93048417e6282a59500bc1a6e3a8c5dcd4e378
TimothyJohnK <Tim@VectorLab.com>
2017-09-19 22:22:52 -0500

    works now oy ve

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 window.addEventListener("load", function() {
-    function dynamicallyBuildDiv() {
-        const arrOfStr = [
-            "Option shift down/up-arrow to copy a line",
-            "Option left/right-arrow key to move one word or character block at a time",
-            "Option up/down arrows to move entire line up or down",
-            "Command any-arrow move all the way to beginning/end of line or document",
-            "Command ?/ comments out a line; command [ indents line left; command ] indents a line right "
-        ];
+    const arrOfStr = [
+        "1. Option shift down/up-arrow to copy a line",
+        "2. Option left/right-arrow key to move one word or character block at a time",
+        "3. Option up/down arrows to move entire line up or down",
+        "4. Command any-arrow move all the way to beginning/end of line or document",
+        "5. Command ?/ comments out a line; command [ indents line left; command ] indents a line right "
+    ];
 
-        for (let i = 0; i >= arrOfStr.length; i++) {
+    function dynamicallyBuildDiv() {
+        for (let i = 0; i <= arrOfStr.length - 1; i++) {
             let newDiv = document.createElement("div");
             newDiv.id = i;
             newDiv.className = "dynamically_built_div";
             newDiv.innerText = arrOfStr[i];
-            document.getElementById("dynamically_built_defs").appendChild(newDiv);
+            document.getElementById("dyna_div_wrapper").appendChild(newDiv);
         }
     }
     dynamicallyBuildDiv();
